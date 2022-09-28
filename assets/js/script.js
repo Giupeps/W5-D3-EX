fc0d5f7c501a4ed97705ee8b5fdbccf04e2abc84
Giupeps <dipisagiuseppe92@gmail.com>
2022-09-28 16:49:47 +0200

    last commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,17 +3,23 @@ let images = [
   '<img class="box" src="./assets/img/dimaio.jpeg">',
   '<img class="box" src="./assets/img/pillon.jpg">',
   '<img class="box" src="./assets/img/sgarbi.jpg">',
+  '<img class="box" src="./assets/img/adinolfi.jpg">'
 ];
 
-function carousel() {
+let interval
+
+function carouselStart() {
   let i = 0;
-  setInterval(() => {
+  interval = setInterval(() => {
     let link = images[i];
     document.getElementById("carousel").innerHTML = link;
     i++;
     if (i == images.length) {
         i=0
     }
-  }, 2000);
+  }, 2500);
 }
 
+function carouselStop() {
+    clearInterval(interval)
+}
